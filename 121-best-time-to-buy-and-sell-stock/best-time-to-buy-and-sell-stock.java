dc7cdb503a157b0341fab1c6dda16f89492bd1e9
Halil Erkan <75100033+halilerkan-cs@users.noreply.github.com>
2024-09-08 11:33:17 +0300

    Time: 2 ms (72.08%) | Memory: 61.5 MB (62.59%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,21 +1,39 @@
 class Solution {
+    //  My Solution:
+    
+    // public int maxProfit(int[] prices) {
+    //     if(prices.length == 0)
+    //         return 0;
+    //     int[] minPricesByIndex = new int[prices.length];
+    //     int[] currentProfit = new int[prices.length];
+    //     minPricesByIndex[0] = prices[0];
+
+    //     for(int i = 1; i < prices.length; i++)
+    //         minPricesByIndex[i] = Math.min(minPricesByIndex[i-1], prices[i]);
+
+    //     for(int i = 0; i < prices.length; i++)
+    //         currentProfit[i] = prices[i] - minPricesByIndex[i];
+
+    //     int maxProfit = 0;
+    //     for(int i = 0; i < prices.length; i++)
+    //         maxProfit = Math.max(maxProfit, currentProfit[i]);
+
+    //     return maxProfit;
+    // }
+
+    //  No Additional Space Approach
     public int maxProfit(int[] prices) {
-        if(prices.length == 0)
-            return 0;
-        int[] minPricesByIndex = new int[prices.length];
-        int[] currentProfit = new int[prices.length];
-        minPricesByIndex[0] = prices[0];
+      int buy = 0;
+      int sell = 1;
+      int maxProfit = 0;
+      while(sell < prices.length) {
+        if(prices[sell] < prices[buy])
+          buy = sell;
+        else
+          maxProfit = Math.max(maxProfit, prices[sell] - prices[buy]);
+        sell++;
+      }
 
-        for(int i = 1; i < prices.length; i++)
-            minPricesByIndex[i] = Math.min(minPricesByIndex[i-1], prices[i]);
-
-        for(int i = 0; i < prices.length; i++)
-            currentProfit[i] = prices[i] - minPricesByIndex[i];
-
-        int maxProfit = 0;
-        for(int i = 0; i < prices.length; i++)
-            maxProfit = Math.max(maxProfit, currentProfit[i]);
-
-        return maxProfit;
+      return maxProfit;
     }
 }
